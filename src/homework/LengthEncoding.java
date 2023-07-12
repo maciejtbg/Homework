66a5d1458a62eadda736a650221a7cf5de57f38b
Maciej Wyrzykowski <maciej_tbg@wp.pl>
2023-07-12 22:27:08 +0200

    Encoding and decoding is working fine and is optimized.

--- src/homework/LengthEncoding.java
+++ src/homework/LengthEncoding.java
@@ -11,32 +11,38 @@ import java.util.List;
 // You can assume the string to be encoded have no digits and consists solely of alphabetic characters.
 // You can assume the string to be decoded is valid.
 public class LengthEncoding {
-    private static String inputString = "AAAABBBCCDAA";
-    public static void main (String[] args){
+    private static String inputString = "AAAABBBCCDA";
+
+    public static void main(String[] args) {
         System.out.println(EncodeString(inputString));
+        System.out.println(DecodeString(EncodeString(inputString)));
     }
 
     private static String EncodeString(String inputString) {
-        String outputString = "";
-        int counter=1;
-        for (int i=1; i<inputString.length(); i++) {
-            if (inputString.charAt(i)==inputString.charAt(i-1)){
+        StringBuilder outputString = new StringBuilder();
+        int counter = 1;
+        for (int i = 1; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == inputString.charAt(i - 1)) {
                 //ta litera jest taka sama jak poprzednia więc dodajemy 1
                 counter++;
-                if (i==inputString.length()-1){
-                    outputString = outputString +(String.valueOf(counter)+inputString.charAt(i));
-                }
-            }else {
+            } else {
                 //koniec ciągu tych liter
-                outputString = outputString +(String.valueOf(counter)+inputString.charAt(i-1));
+                outputString.append(String.valueOf(counter) + inputString.charAt(i - 1));
                 //ta litera jest inna niż poprzednia, więc tej poprzedniej jest ilość = 1
-                counter=1;
-                if (i==inputString.length()-1){
-                    outputString = outputString +(String.valueOf(counter)+inputString.charAt(i));
-                }
+                counter = 1;
             }
         }
+        outputString.append(String.valueOf(counter) + inputString.charAt(inputString.length() - 1));
+        return outputString.toString();
+    }
 
-        return outputString;
+    private static String DecodeString(String inputString) {
+        StringBuilder outputString = new StringBuilder();
+        for (int i = 0; i < inputString.length() - 1; i += 2) {
+            int CharNum = Character.getNumericValue(inputString.charAt(i));
+            char ThisChar = inputString.charAt(i+1);
+            outputString.append(String.valueOf(ThisChar).repeat(CharNum));
+        }
+        return outputString.toString();
     }
 }
